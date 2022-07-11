cfdc36036f3abdeb1498b31d56294ba818563b76
Rinasham <rinashamau@gmail.com>
2022-07-11 19:32:50 +1000

    add localhost to allowed hosts

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['django-api-tasks.herokuapp.com']
+ALLOWED_HOSTS = ['django-api-tasks.herokuapp.com','localhost']
 
 
 # Application definition
